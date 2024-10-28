f5f69b8b03e5cfa7dbfa83874d9614d0eefc1857
Nageswari16 <153205021+Nageswari16@users.noreply.github.com>
2024-10-28 12:02:30 +0530

    jj

--- fesm2022/pics-core-dynamic.mjs
+++ fesm2022/pics-core-dynamic.mjs
@@ -1,4 +1,4 @@
-export { o as CardiDynamicModule, n as DynamicComponent, D as DynamicPageCleanupComponent, a as DynamicPageComponent, i as DynamicSearchComponent, h as DynamicSearchModule, m as DynamicService, k as DynamicTabPageService, l as DynamicsearchService, j as DynmicTabPageModule } from './pics-core-dynamic-pics-core-dynamic-1115493e.mjs';
+export { o as CardiDynamicModule, n as DynamicComponent, D as DynamicPageCleanupComponent, a as DynamicPageComponent, i as DynamicSearchComponent, h as DynamicSearchModule, m as DynamicService, k as DynamicTabPageService, l as DynamicsearchService, j as DynmicTabPageModule } from './pics-core-dynamic-pics-core-dynamic-82f1033f.mjs';
 import '@angular/core';
 import 'rxjs';
 import '@angular/router';
