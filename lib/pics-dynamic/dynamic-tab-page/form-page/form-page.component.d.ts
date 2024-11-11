01573c293aa5e394d343bba17cb110cfe5d747d0
Nageswari16 <153205021+Nageswari16@users.noreply.github.com>
2024-11-11 19:44:00 +0530

    SE

--- lib/pics-dynamic/dynamic-tab-page/form-page/form-page.component.d.ts
+++ lib/pics-dynamic/dynamic-tab-page/form-page/form-page.component.d.ts
@@ -22,6 +22,7 @@ import { ComponentState } from '../../@shared/master-view-state/master-view.stat
 import { Observable, Subscription } from 'rxjs';
 import { AppState } from '../../@core/core.state';
 import { VideoSpeechContentService } from './video-speech-content.service';
+import { ocrResponseService } from './ocr-response.service';
 import * as i0 from "@angular/core";
 export declare class FormPageComponent implements OnInit, OnDestroy, AfterViewInit {
     router: Router;
@@ -80,6 +81,7 @@ export declare class FormPageComponent implements OnInit, OnDestroy, AfterViewIn
     dataStore: DataStoreService;
     speechRecognitionService: SpeechRecognitionService;
     videoSpeechContentService: VideoSpeechContentService;
+    ocrResponseService: ocrResponseService;
     uploadService: NgxfUploaderService;
     ocr: OCRService;
     appointmentList: Promise<boolean> | undefined;
@@ -180,8 +182,6 @@ export declare class FormPageComponent implements OnInit, OnDestroy, AfterViewIn
     customEvents(evt: any): void;
     ngOnDestroy(): void;
     uploadFile(file: File | FileError): void;
-    processResponseData(file: any): void;
-    processOCRResponse(result: any): void;
     prepareFormKeyLabel(json: any): void;
     financeCustomEventsFunctionality(event: any): void;
     routeToGrid(val: any): void;
